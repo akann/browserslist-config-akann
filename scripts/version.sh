64ccbc02dff3749d863c7d3e8d98c2b36957f965
Akan Nkweini <akan2000@gmail.com>
2019-02-13 15:54:36 +0000

    version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,19 @@
-
+REMOTE_VERION=$(npm info browserslist-config-akann version)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-TAG_NAME=$(echo "$BRANCH" | tr 'A-Z' 'a-z' | tr -cd '[[:alnum:]]')
+TAG_NAME=$(echo "${BRANCH}" | tr 'A-Z' 'a-z' | tr -cd '[[:alnum:]]')
 
-if [ "x-$BRANCH" = "x-master" ]; then
-    npm version patch
+if [ "x-${BRANCH}" = "x-master" ]; then
+    npm version --no-git-tag-version --new-version ${REMOTE_VERION}
+    npm --no-git-tag-version version patch
 else 
-    npm version prerelease --preid=$TAG_NAME
+    CURRENT_VERSION=$(node -pe "require('./package.json').version")
+    CURRENT_TAG_NAME=$(echo ${CURRENT_VERSION} | sed "s/[0-9].*\.[0-9].*\.[0-9].*-\(${TAG_NAME}\)\.[0-9].*$/\1/")
+
+    if [ "x$CURRENT_TAG_NAME" != "x${TAG_NAME}" ]; then
+        npm version --no-git-tag-version --new-version ${REMOTE_VERION}
+    fi
+    
+    npm version prerelease --preid=${TAG_NAME} -m 'version++'
 fi
 
 
