b6f1d045860947e764b79f5d32ee620f7e39945d
Akan Nkweini <akan2000@gmail.com>
2019-02-14 12:48:03 +0000

    release

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,6 @@ fi
 TAG_NAME=$(echo "${BRANCH_NAME}" | tr 'A-Z' 'a-z' | tr -cd '[[:alnum:]]')
 
 push_tag () {
-    git checkout $BRANCH_NAME
     NEW_VERSION=$(node -pe "require('./package.json').version")
 
     git add package.json
