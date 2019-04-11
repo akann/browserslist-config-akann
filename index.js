e04fbf68987e8511527f7f1b05e5a7174e968496
Akan Nkweini <akan2000@gmail.com>
2019-04-11 14:05:45 +0100

    removed IE11

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ module.exports = [
 	'last 2 Firefox major versions',
 	'last 2 Edge major versions',
 	'last 2 Safari major versions',
-	'ie 11',
 	'last 3 Android major versions',
 	'last 3 ChromeAndroid major versions',
 	'last 2 iOS major versions'
